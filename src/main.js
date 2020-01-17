c95f4217caae52585a023a8fb2128b49b593b636
pierre.burton91 <pierre.burton91@gmail.com>
2020-01-17 19:20:28 +0100

    remove animejs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from "vue";
 import "./shared/sass/theme.scss";
-import anime from "animejs/lib/anime.es.js";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
-
-Vue.prototype.$anime = anime;
 
 new Vue({
   render: h => h(App)
