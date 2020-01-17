f4a29038c5c8942acaa5c13b95b56b61fab49757
pierre.burton91 <pierre.burton91@gmail.com>
2020-01-17 17:56:39 +0100

    update publicPath

---
+++ docs/precache-manifest.8e8fa09134f0017fa1289bbcc693cc7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8646f283965e5157dce2",
+    "url": "/wodisher/css/app.505e9c7c.css"
+  },
+  {
+    "revision": "6f3600e166aee4521a0f7516a325ffd7",
+    "url": "/wodisher/index.html"
+  },
+  {
+    "revision": "8646f283965e5157dce2",
+    "url": "/wodisher/js/app.91c7db69.js"
+  },
+  {
+    "revision": "ba0426f86539bf68d6e0",
+    "url": "/wodisher/js/chunk-vendors.a36ecbdd.js"
+  },
+  {
+    "revision": "5b2bb7ddfe266aaae79bc4a4c130e658",
+    "url": "/wodisher/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wodisher/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f5fea677dfc18f684ef4fbe8f9fc06e.js"
+  "/wodisher/precache-manifest.8e8fa09134f0017fa1289bbcc693cc7c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "wodisher"});
